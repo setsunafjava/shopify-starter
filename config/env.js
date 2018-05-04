305ead40bbf1d7e82a395853bcb3f976302f883e
Vu Tien Dat <setsunafjava@gmail.com>
2018-05-04 03:29:55 +0000

    a

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 const env = Object.assign({
   NAME: 'Shopify Embedded PEX Starter',
   PORT: 8080,
-  DATABASE: 'mongodb://root:1@cluster0-shard-00-00-dyso2.mongodb.net:27017,cluster0-shard-00-01-dyso2.mongodb.net:27017,cluster0-shard-00-02-dyso2.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+  DATABASE: 'mongodb://abc:123@54.169.153.46:27017/test',
   URL: 'https://shopify-starter-datvt4.c9users.io',
   SHOPIFY_API_KEY: 'e0ca39c53d7380d71549c3f9af595cf9',
   SHOPIFY_API_SECRET: 'ef19134c8c2793da25b4241476511113',
